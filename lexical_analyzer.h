349c8ccb71d7699e8dc5afcda5cab7e1b9e2ebdf
NedoCPP <formassivspam@mail.ru>
2017-10-14 19:56:51 +0300

    once again...

--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -2,9 +2,8 @@
 #define LEXICAL_ANALYZER_H
 #include "Trie_imp.h"
 #include <string>
-#include <locale>
 #include<fstream>
-#include <algorithm>
+
 
 void split_the_string(std::string input)
 {
@@ -14,7 +13,7 @@ void split_the_string(std::string input)
  std::string SF;
  std::string IF;
  bool escape;
- std::locale locale;
+
 
  setFile.open("set.txt");
   bool ByteOrderMark=true;
@@ -30,50 +29,73 @@ void split_the_string(std::string input)
     trie.insert(std::string(SF.begin(),SF.end()));
   }
  setFile.close();
+///////////////////////////////////////////////////
+ ByteOrderMark=true;
+  std::string buff;
+   std::string skip;
+   std::vector<std::string> Found;
 
-ByteOrderMark=true;
- inputFile.open(input);
-  while(std::getline(inputFile,IF))
-  {
-   std::string str;
+  inputFile.open(input);
+   while(std::getline(inputFile,buff))
+   {
     if(ByteOrderMark)
     {
-      str=IF.substr(3,IF.size()-3);
+     buff.erase(0,3);
       ByteOrderMark=false;
     }
-    else
-     str=IF.substr(0,IF.size());
-///////////////////////////////////////////////
-  str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-   auto found=str.find('/');
 
 
-  if(!escape)
-  {
-   if(found<str.size()-1&&str[found+1]=='/')
-   {
-    trie.lookahead(std::string(str.begin(),str.begin()+found));
-    continue;
-   }
-   else if(found<str.size()-1&&str[found+1]=='*')
-   {
-    trie.lookahead(std::string(str.begin(),str.begin()+found));
-    escape=true;
-     continue;
-   }
-  }
-  else
-  {
-     if(found<=str.size()-1&&found>=1&&str[found-1]=='*')
+       for(size_t i=0;i<buff.size()-1;i++)
+       {
+         if(escape)
+         {
+          if(buff[i]=='*'&&buff[i+1]=='/')
+          {
+           i++;
+           escape=false;
+            continue;
+          }
+          else continue;
+         }
+
+        if(buff[i]!=' ')
+        {
+              if(buff[i]=='/'&&buff[i+1]=='/') break;
+         else if(buff[i]=='/'&&buff[i+1]=='*')
+              {
+               escape=true;
+               if(!skip.empty())
+               {
+                Found.emplace_back(skip);
+                 skip.clear();
+               }
+              }
+           else
+              {
+                  skip+=buff[i];
+              }
+        }
+        else if(buff[i]==' '&&!skip.empty())
+        {
+         Found.emplace_back(skip);
+          skip.clear();
+        }
+
+       }
+     if(!skip.empty())
      {
-      escape=false;
-       trie.lookahead(std::string(str.begin()+found+1,str.end()));//???
+      Found.emplace_back(skip);
+      skip.clear();
      }
+   }
 
-  }
- }
+   for(auto i:Found)
+   {
+      trie.lookahead(i);
+   }
+
+  inputFile.close();
 }
-
 
 
 
